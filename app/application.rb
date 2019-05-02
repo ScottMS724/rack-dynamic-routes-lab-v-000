782acf7fd78533d85b7c14624416fbab305aaa4a
Scott Silverman <scottms724@gmail.com>
2019-05-02 18:00:19 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,7 @@ class Application
       
       item_name = req.path.split("/items/").last
       
-      if @@items.include?(item_name)
+      if @@items.find
         resp.write item_name.price 
       else 
         resp.write "Item not found"
