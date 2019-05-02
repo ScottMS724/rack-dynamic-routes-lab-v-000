aba1f5e945da398a608b07ef288fa97e827afa4a
Scott Silverman <scottms724@gmail.com>
2019-05-02 18:04:41 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,7 @@ class Application
       
       item_name = req.path.split("/items/").last
       
-      if @@items.find { |item| item.name == item_name }
+      if item = @@items.find { |item| item.name == item_name }
         resp.write item_name.price 
       else 
         resp.write "Item not found"
